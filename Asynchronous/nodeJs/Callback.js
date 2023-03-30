28b03198dbb307517e3f95f559102142db257783
bagasharfianto <bagasharfianto00@gmail.com>
2023-03-30 10:46:35 +0700

    CallBack Beres

--- Asynchronous/nodeJs/Callback.js
+++ Asynchronous/nodeJs/Callback.js
@@ -1,1 +1,24 @@
-fu
+function getUsers(isOffline,callback) {
+  setTimeout(() => {
+    const users = ["Bambang", "Kuncoro", "Srimulim"];
+    if (isOffline) {
+      callback(new Error("cannot retrieve users due offline"), null);
+      return;
+    }
+    callback(null,users);
+   }, 3000);
+}
+//meng ekstraksi fungsi callback tersebut
+function callbackUsers(error,users) {
+  if (error) {
+    console.log('process failed:', error.message);
+    return;
+  }
+  
+  users.forEach(user => {
+    console.log(user);
+  });
+}
+
+
+getUsers(true,callbackUsers);
